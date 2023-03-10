57775576445a6008d58eb5c52ba74da315daa41e
IzePhanthakarn <izephanthakarn@hotmail.com>
2023-03-10 12:42:38 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+import * as faker from "faker";
+import * as commander from "commander";
+
+const program = new commander.Command();
+
+program
+  .version("1.0.0")
+  .description("CLI tool for generate Lorem Ipsum Text");
+
+// command generate
+program
+  .command("generate")
+  .alias("g")
+  .description("Generate Lorem Ipsum Text")
+  .option("-p, --paragraphs <number>", "Number of paragraphs to create", "1")
+  .action((command) => {
+    const { paragraphs } = command;
+
+    // generate LoremIpsum
+    for (let i = 0; i < parseInt(paragraphs); i++) {
+      console.log(faker.lorem.paragraph());
+    }
+  });
+
+// run commander command
+program.parse(process.argv);
