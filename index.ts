18197e5d269d6b8d8d3b48d1404ee67a47760884
IzePhanthakarn <izephanthakarn@hotmail.com>
2023-03-10 13:21:44 +0700

    ♻️ Refactor code

--- index.ts
+++ index.ts
@@ -4,23 +4,23 @@ import * as commander from "commander";
 const program = new commander.Command();
 
 program
-  .version("1.0.0")
-  .description("CLI tool for generate Lorem Ipsum Text");
+    .version("1.0.0")
+    .description("CLI tool for generate Lorem Ipsum Text");
 
 // command generate
 program
-  .command("generate")
-  .alias("g")
-  .description("Generate Lorem Ipsum Text")
-  .option("-p, --paragraphs <number>", "Number of paragraphs to create", "1")
-  .action((command) => {
-    const { paragraphs } = command;
+    .command("generate")
+    .alias("g")
+    .description("Generate Lorem Ipsum Text")
+    .option("-p, --paragraphs <number>", "Number of paragraphs to create", "1")
+    .action((command) => {
+        const { paragraphs } = command;
 
-    // generate LoremIpsum
-    for (let i = 0; i < parseInt(paragraphs); i++) {
-      console.log(faker.lorem.paragraph());
-    }
-  });
+        // generate LoremIpsum
+        for (let i = 0; i < parseInt(paragraphs); i++) {
+            console.log(faker.lorem.paragraph(), "\n");
+        }
+    });
 
 // run commander command
 program.parse(process.argv);
